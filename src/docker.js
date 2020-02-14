28e63a80fe4af264427c99b8976a6d05ce36588b
David Vandensteen <david.vandensteen@leroymerlin.fr>
2020-02-14 10:47:21 +0100

    docker cli fix

--- src/docker.js
+++ src/docker.js
@@ -1,54 +1,33 @@
 #!/usr/bin/env node
-"use strict";
 
-var _v = _interopRequireDefault(require("uuid/v4"));
+import uuid from 'uuid/v4';
+import Cli from './class/Cli';
+import VirtualBox from './class/Cli/VirtualBox';
 
-var _Cli = _interopRequireDefault(require("./class/Cli"));
-
-var _VirtualBox = _interopRequireDefault(require("./class/Cli/VirtualBox"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-var _console = console,
-    error = _console.error;
-var input = process.argv.slice(2);
-
-var args = _Cli["default"].escape(input).join(' ');
+const { error } = console;
+const input = process.argv.slice(2);
+const args = Cli.escape(input).join(' ');
 
 if (input.length > 1 && input[0] === 'build') {
-  var id = (0, _v["default"])();
+  const id = uuid();
 
-  _VirtualBox["default"].share('ocm', {
+  VirtualBox.share('ocm', {
     name: id,
     hostpath: process.cwd(),
     readonly: true,
-    "transient": true
-  }).then(function () {
-    return _Cli["default"].exec("mkdir -p /tmp/ocm-volatile/".concat(id));
-  }).then(function () {
-    return _Cli["default"].exec("sudo mount -t vboxsf -o gid=vboxsf ".concat(id, " /tmp/ocm-volatile/").concat(id));
-  }).then(function () {
-    return _Cli["default"].exec("cd /tmp/ocm-volatile/".concat(id, " && podman ").concat(args));
-  }).then(function () {
-    return _Cli["default"].exec("sudo umount /tmp/ocm-volatile/".concat(id));
-  }).then(function () {
-    return _Cli["default"].exec("rmdir /tmp/ocm-volatile/".concat(id));
-  }).then(function () {
-    return _VirtualBox["default"].unshare('ocm', {
+    transient: true,
+  })
+    .then(() => Cli.exec(`mkdir -p /tmp/ocm-volatile/${id}`))
+    .then(() => Cli.exec(`sudo mount -t vboxsf -o gid=vboxsf ${id} /tmp/ocm-volatile/${id}`))
+    .then(() => Cli.exec(`cd /tmp/ocm-volatile/${id} && sudo docker ${args}`))
+    .then(() => Cli.exec(`sudo umount /tmp/ocm-volatile/${id}`))
+    .then(() => Cli.exec(`rmdir /tmp/ocm-volatile/${id}`))
+    .then(() => VirtualBox.unshare('ocm', {
       name: id,
-      "transient": true
-    });
-  })["catch"](function (err) {
-    error(err.message);
-  });
-}
-
-if (input.length > 1 && input[0] === 'network') {
-  _Cli["default"].exec("sudo docker ".concat(args))["catch"](function (err) {
-    return error(err.message);
-  });
+      transient: true,
+    }))
+    .catch((err) => { error(err.message); });
 } else {
-  _Cli["default"].exec("docker ".concat(args))["catch"](function (err) {
-    return error(err.message);
-  });
+  Cli.exec(`sudo docker ${args}`)
+    .catch((err) => error(err.message));
 }
